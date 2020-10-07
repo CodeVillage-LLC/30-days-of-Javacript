718eb5140d44d28d05be52c23c85d9bf262f017f
Abdulsamii Ajala <kgasta@gmail.com>
2020-10-07 16:58:07 +0100

    feat: Day1

---
+++ 01-Day/script.js
@@ -0,0 +1,61 @@
+/**
+ * Exercise 1
+*/
+// comment can make code readable
+
+/**
+ * Exercise 2
+*/
+
+// Welcome to 30DaysOfJavaScript
+
+/**
+ * Exercise 3
+ */
+
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
+
+/**
+ * 
+*/
+
+
+// console.log(typeof 'Ajala')
+// console.log(typeof 'Asabeneh') // string
+// console.log(typeof 5) // number
+// console.log(typeof true) // boolean
+// console.log(typeof null) // object type
+// console.log(typeof undefined) // undefined
+
+
+// /* 
+// multiple 
+// line comment
+// */
+
+// // Declaring different variables of different data types
+// let firstName = 'Asabeneh' // first name of a person
+// let lastName = 'Yetayeh' // last name of a person
+// let country = 'Finland' // country
+// let city = 'Helsinki' // capital city
+// // let age = 100 // age in years
+// let isMarried = true
+
+// console.log(firstName, lastName, country, city, isMarried)
+
+// // Declaring variables with number values
+// let age = 100 // age in years
+// const gravity = 9.81 // earth gravity  in m/s2
+// const boilingPoint = 100 // water boiling point, temperature in oC
+// const PI = 3.142 // geometrical constant
+
+// console.log(gravity, boilingPoint, PI)
+
+// let name = 'Jalasem',
+//   job = 'Farmer',
+//   live = 'Ile epo'
+
+// console.log(name, job, live)
---
+++ 01-Day/variable.js
@@ -0,0 +1,56 @@
+/**
+ * Exercise 4
+ */
+
+let name = 'Jalasem'
+let isAFather = false
+let noOfKids
+let bully = null
+
+console.log(name, isAFather, noOfKids, bully)
+
+/**
+ * exercise 5
+ */
+console.log(typeof name, typeof isAFather, typeof noOfKids, typeof bully)
+
+/**
+ * exercise 6
+ */
+
+ let man1, man2, man3, man4
+
+ /**
+ * exercise 7
+ */
+ let man5 = 'Saka', man6 = 'Alao', man7 = 'Muniru', man8 = 'Muktairu'
+
+ /**
+ * exercise 8
+ */
+let firstName = 'Abdulsamii'
+let lastName = 'Ajala'
+let maritalStatus = 'Double'
+let country = 'Ethiopia'
+let age = 16
+
+/**
+ * exercise 9
+ */
+
+let first_name = 'Abdulsamii',
+  last_name = 'Ajala',
+  marital_status = 'Double',
+  Country = 'Ethiopia',
+  Age = 16
+
+
+/**
+ * exercise 10
+ */
+
+let myAge = 20
+let yourAge = 30
+
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
